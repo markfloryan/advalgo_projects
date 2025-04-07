db57954f3512dd02d3a5d75b6f31e381e7ec0b37
Eddie <EdwardHSLi18@gmail.com>
2025-04-07 11:32:07 -0400

    Add test cases and comments

--- projects/11-burrowsWheelerTransform/programmingChallenge/solutions/pcSol_cpp.cpp
+++ projects/11-burrowsWheelerTransform/programmingChallenge/solutions/pcSol_cpp.cpp
@@ -11,6 +11,10 @@ const ll LINF = LLONG_MAX - 1;
 
 pair<vector<int>, map<int, int>> rankBwt(vector<int> &bw)
 {
+    /*
+     * Given BWT array bw, return parallel list of B-ranks.
+     * Also returns tots: map from value to # times it appears.
+     */
     map<int, int> tots;
     vector<int> ranks;
 
@@ -24,6 +28,9 @@ pair<vector<int>, map<int, int>> rankBwt(vector<int> &bw)
 
 map<int, pair<int, int>> firstCol(map<int, int> &tots)
 {
+    /*
+     * Return map from value to the range of rows prefixed by that value
+     */
     map<int, pair<int, int>> first;
     int totc = 0;
     for (const auto &[val, count] : tots)
@@ -36,6 +43,9 @@ map<int, pair<int, int>> firstCol(map<int, int> &tots)
 
 vector<int> reverseBwt(vector<int> &bw)
 {
+    /*
+     * Make original list from BWT list bw
+     */
     pair<vector<int>, map<int, int>> rt = rankBwt(bw);
     vector<int> ranks = rt.first;
     map<int, int> tots = rt.second;
@@ -72,13 +82,18 @@ void solve()
     }
     int last = 0;
     vector<pair<int, int>> gaps;
+    /*
+     * From the sorted elements, get consecutive elements, and if there is a gap,
+     * add it to our gaps array
+     */
     for (int i : elements)
     {
         if (last + 1 <= i - 1)
         {
+            // add 1 to the left, and subtract 1 to right to just get elements in between
             gaps.push_back({last + 1, i - 1});
         }
-        gaps.push_back({i, i});
+        gaps.push_back({i, i}); // # also make sure to check the elements in the actual array
         last = i;
     }
     int mx = 0;
@@ -88,9 +103,15 @@ void solve()
     }
     if (k > mx)
     {
+        // make sure to add a gap between largest element and K
         gaps.push_back({mx + 1, k});
     }
     int zero = find(arr.begin(), arr.end(), 0) - arr.begin();
+    /*
+     * Test each gap
+     * If the element works, then we add to our answer the number of elements in
+     * that gap, as all elements will work if one element works
+     */
     int ans = 0;
     for (pair<int, int> item : gaps)
     {
@@ -99,6 +120,7 @@ void solve()
         vector<int> original = reverseBwt(arr);
         if (original.size() == arr.size())
         {
+            // the original array is valid if there are no missing elements
             ans += item.second - item.first + 1;
         }
     }
--- projects/11-burrowsWheelerTransform/programmingChallenge/solutions/pcSol_java.java
+++ projects/11-burrowsWheelerTransform/programmingChallenge/solutions/pcSol_java.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class pcSol_java {
     public static List<Integer> rankBwt(List<Integer> bw, Map<Integer, Integer> tots) {
+        /*
+         * Given BWT array bw, return parallel list of B-ranks.
+         * Also returns tots: map from value to # times it appears.
+         */
         List<Integer> ranks = new ArrayList<>();
         for (int val : bw) {
             tots.putIfAbsent(val, 0);
@@ -12,6 +16,9 @@ public class pcSol_java {
     }
 
     public static Map<Integer, int[]> firstCol(Map<Integer, Integer> tots) {
+        /*
+         * Return map from value to the range of rows prefixed by that value
+         */
         Map<Integer, int[]> first = new TreeMap<>();
         int totc = 0;
         for (int val : new TreeSet<>(tots.keySet())) {
@@ -23,6 +30,9 @@ public class pcSol_java {
     }
 
     public static List<Integer> reverseBwt(List<Integer> bw) {
+        /*
+         * Make original list from BWT list bw
+         */
         Map<Integer, Integer> tots = new HashMap<>();
         List<Integer> ranks = rankBwt(bw, tots);
         Map<Integer, int[]> first = firstCol(tots);
@@ -55,7 +65,12 @@ public class pcSol_java {
         Collections.sort(elements);
 
         List<int[]> gaps = new ArrayList<>();
+        /*
+         * From the sorted elements, get consecutive elements, and if there is a gap,
+         * add it to our gaps array
+         */
         for (int i = 1; i < n - 1; i++) {
+            // add 1 to the left, and subtract 1 to right to just get elements in between
             int f = elements.get(i) + 1;
             int s = elements.get(i + 1) - 1;
             if (f <= s) {
@@ -63,11 +78,11 @@ public class pcSol_java {
             }
         }
 
-        if (k > elements.get(n - 1)) {
+        if (k > elements.get(n - 1)) { // make sure to add a gap between largest element and K
             gaps.add(new int[] { elements.get(n - 1) + 1, k });
         }
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) { // # also make sure to check the elements in the actual array
             if (!elements.get(i).equals(elements.get(i - 1))) {
                 gaps.add(new int[] { elements.get(i), elements.get(i) });
             }
@@ -76,10 +91,15 @@ public class pcSol_java {
         int zero = arr.indexOf(0);
         int ans = 0;
 
+        /*
+         * Test each gap
+         * If the element works, then we add to our answer the number of elements in
+         * that gap, as all elements will work if one element works
+         */
         for (int[] item : gaps) {
             arr.set(zero, item[0]);
             List<Integer> original = reverseBwt(arr);
-            if (original.size() == arr.size()) {
+            if (original.size() == arr.size()) { // # the original array is valid if there are no missing elements
                 ans += item[1] - item[0] + 1;
             }
         }
