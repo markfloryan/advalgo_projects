92dcfa8b5e841a25b9f851afdbf7f8dac9b594cc
grady-h <116502434+grady-h@users.noreply.github.com>
2025-04-19 12:39:13 -0400

    implement assign and get segtree

--- projects/05-segmentTreeRangeUpdates/implementation/segTreeRangeUpdates.cpp
+++ projects/05-segmentTreeRangeUpdates/implementation/segTreeRangeUpdates.cpp
@@ -8,6 +8,7 @@ class Node {
     public:
         int l, r;
         int val;
+        bool marked;  // for range assignments
         Node* l_child;
         Node* r_child;
 
@@ -83,8 +84,8 @@ class SegTreeAdditionAndGet {
             build(a, root);
         }
 
-        // query simply returns `a[pos]`
-        int query(Node* cur_node, int pos) {
+        // get simply returns `a[pos]`
+        int get(Node* cur_node, int pos) {
             // base case: if we're at a leaf node, just return it's value
             if (cur_node->l == cur_node->r) {
                 return cur_node->val;
@@ -97,9 +98,9 @@ class SegTreeAdditionAndGet {
             */
             if (pos <= cur_node->l_child->r) {
                 // add cur_node.val to the tally, query child
-                return cur_node->val + query(cur_node->l_child, pos);
+                return cur_node->val + get(cur_node->l_child, pos);
             } else {
-                return cur_node->val + query(cur_node->r_child, pos);
+                return cur_node->val + get(cur_node->r_child, pos);
             }
         }
 
@@ -160,5 +161,95 @@ class SegTreeAdditionAndGet {
             made any queries yet, so we just set to 0 for now.
             */
             cur_node->val = 0;
+        }
+};
+
+
+
+
+
+
+class SegTreeAssignAndGet {
+    public:
+        SegTreeAssignAndGet(vector<int>& a) {
+            Node* root = new Node(0, a.size() - 1);
+            build(a, root);
+        }
+
+        // get simply returns `a[pos]`
+        int get(Node* cur_node, int pos) {
+            // base case: we're at a leaf node
+            if (cur_node->l == cur_node->r) {
+                return cur_node->val;
+            }
+            // push our info down to our children if necessary
+            push(cur_node);
+            // then simply query on the correct child
+            if (pos <= cur_node->l_child->r) {
+                return get(cur_node->l_child, pos);
+            } else {
+                return get(cur_node->r_child, pos);
+            }
+        }
+
+        // 
+        void update(Node* cur_node, int l, int r, int new_val) {
+            // degenerate base case
+            if (l > r) {
+                return;
+            }
+            /*
+            if the query range matches the node range exactly,
+            set the node's value and mark it
+            */
+            if (l == cur_node->l && r == cur_node->r) {
+                cur_node->val = new_val;
+                cur_node->marked = true;
+                return;
+            }
+            /*
+            first, we need to push our info down to our children if necessary.
+            this is our "lazy update" -- we only do this when we need to go
+            deeper into the tree
+            */
+            push(cur_node);
+            // now just call update on our children, as per usual
+            update(cur_node->l_child, l, min(r, cur_node->l_child->r), new_val);
+            update(cur_node->r_child, max(l, cur_node->r_child->l), r, new_val);
+        }
+
+    private:
+        void push(Node* node){
+            if (node->marked) {
+                // push our value down to our children
+                node->l_child->val = node->r_child->val = node->val;
+                // mark the children to indicate they now have contiguous range values
+                node->l_child->marked = node->r_child->marked = true;
+                // unmark ourselves to indicate we don't have a contiguous range value anymore
+                node->marked = false;
+            }
+        }
+
+        void build(vector<int>& a, Node* cur_node) {
+            if (cur_node->l == cur_node->r) {
+                // we're still just storing the array value in the leaf nodes
+                cur_node->val = a[cur_node->l];
+                return;
+            }
+
+            int mid = (cur_node->l + cur_node->r) / 2;
+            cur_node->l_child = new Node(cur_node->l, mid);
+            cur_node->r_child = new Node(mid + 1, cur_node->r);
+
+            build(a, cur_node->l_child);
+            build(a, cur_node->r_child);
+
+            /*
+            now the node value store whatever the contiguous assignment
+            value is on that segment, if it has one. This will be set 
+            IF AND ONLY IF the node is marked. So, to start, we just 
+            set it to -1 (nil)
+            */
+            cur_node->val = -1;
         }
 };
