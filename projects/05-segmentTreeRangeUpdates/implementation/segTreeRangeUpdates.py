e68da6a3801bc31f28d2aace22ce9fc3e7d73b70
Edward Chen <edward9065@gmail.com>
2025-04-20 19:04:43 -0400

    complete python implementation and test

--- projects/05-segmentTreeRangeUpdates/implementation/segTreeRangeUpdates.py
+++ projects/05-segmentTreeRangeUpdates/implementation/segTreeRangeUpdates.py
@@ -1,6 +1,6 @@
 class segment_tree_addition_and_get:
     def __init__(self, input_array):
-        tree = [0]*(len(input_array)*4)
+        self.tree = [0]*(len(input_array)*4)
         self.build(input_array, 1, 0, len(input_array)-1)
     
     def build(self, input_array, curr_vertex, segment_left, segment_right):
@@ -12,29 +12,29 @@ class segment_tree_addition_and_get:
             self.build(input_array, curr_vertex*2+1, segment_middle + 1, segment_right)
             self.tree[curr_vertex] = 0
     
-    def add_to_range(self, curr_vertex, segment_left, segment_right, left, right, num_to_add):
+    def update(self, curr_vertex, segment_left, segment_right, left, right, num_to_add):
         if left > right:
             return
         if left == segment_left and right == segment_right:
             self.tree[curr_vertex] += num_to_add
         else:
             segment_middle = (segment_left + segment_right) // 2
-            self.add_to_range(curr_vertex*2, segment_left, segment_middle, left, min(right, segment_middle), num_to_add)
-            self.add_to_range(curr_vertex*2+1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_add)
+            self.update(curr_vertex*2, segment_left, segment_middle, left, min(right, segment_middle), num_to_add)
+            self.update(curr_vertex*2+1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_add)
 
     def get(self, curr_vertex, segment_left, segment_right, position):
         if segment_left == segment_right:
             return self.tree[curr_vertex]
         segment_middle = (segment_left + segment_right) // 2
         if position <= segment_middle:
-            return self.tree[curr_vertex] + self.get(curr_vertex * 2, segment_left, segment_right, position)
+            return self.tree[curr_vertex] + self.get(curr_vertex * 2, segment_left, segment_middle, position)
         else:
             return self.tree[curr_vertex] + self.get(curr_vertex * 2 + 1, segment_middle + 1, segment_right, position)
 
 class segment_tree_assignment_and_get:
     def __init__(self, input_array):
-        tree = [0]*(len(input_array)*4)
-        marked = [False]*(len(input_array)*4)
+        self.tree = [0]*(len(input_array)*4)
+        self.marked = [False]*(len(input_array)*4)
         self.build(input_array, 1, 0, len(input_array)-1)
     
     def build(self, input_array, curr_vertex, segment_left, segment_right):
@@ -52,7 +52,7 @@ class segment_tree_assignment_and_get:
             self.marked[vertex*2] = self.marked[vertex*2+1] = True
             self.marked[vertex] = False
 
-    def assign_to_range(self, curr_vertex, segment_left, segment_right, left, right, num_to_assign):
+    def update(self, curr_vertex, segment_left, segment_right, left, right, num_to_assign):
         if left > right:
             return
         if left == segment_left and right == segment_right:
@@ -60,8 +60,8 @@ class segment_tree_assignment_and_get:
             self.marked[curr_vertex] = True
         else:
             segment_middle = (segment_left + segment_right) // 2
-            self.assign_to_range(curr_vertex * 2, segment_left, segment_middle, left, min(right, segment_middle), num_to_assign)
-            self.assign_to_range(curr_vertex * 2 + 1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_assign)
+            self.update(curr_vertex * 2, segment_left, segment_middle, left, min(right, segment_middle), num_to_assign)
+            self.update(curr_vertex * 2 + 1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_assign)
 
     def get(self, curr_vertex, segment_left, segment_right, position):
         if segment_left == segment_right:
@@ -69,14 +69,14 @@ class segment_tree_assignment_and_get:
         self.push(curr_vertex)
         segment_middle = (segment_left + segment_right) // 2
         if position <= segment_middle:
-            return self.get(curr_vertex * 2, segment_left, segment_right, position)
+            return self.get(curr_vertex * 2, segment_left, segment_middle, position)
         else:
             return self.get(curr_vertex * 2 + 1, segment_middle + 1, segment_right, position)
 
 class segment_tree_addition_and_max:
     def __init__(self, input_array):
-        tree = [0]*(len(input_array)*4)
-        lazy = [0]*(len(input_array)*4)
+        self.tree = [0]*(len(input_array)*4)
+        self.lazy = [0]*(len(input_array)*4)
         self.build(input_array, 1, 0, len(input_array)-1)
 
     def push(self, vertex):
@@ -93,9 +93,9 @@ class segment_tree_addition_and_max:
             segment_middle = (segment_left + segment_right) // 2
             self.build(input_array, curr_vertex*2, segment_left, segment_middle)
             self.build(input_array, curr_vertex*2+1, segment_middle + 1, segment_right)
-            self.tree[curr_vertex] = 0
+            self.tree[curr_vertex] = max(self.tree[curr_vertex*2], self.tree[curr_vertex*2+1])
     
-    def add_to_range(self, curr_vertex, segment_left, segment_right, left, right, num_to_add):
+    def update(self, curr_vertex, segment_left, segment_right, left, right, num_to_add):
         if left > right:
             return
         if left == segment_left and right == segment_right:
@@ -104,19 +104,56 @@ class segment_tree_addition_and_max:
         else:
             self.push(curr_vertex)
             segment_middle = (segment_left + segment_right) // 2
-            self.add_to_range(curr_vertex*2, segment_left, segment_middle, left, min(right, segment_middle), num_to_add)
-            self.add_to_range(curr_vertex*2+1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_add)
-            self.tree = max(self.tree[curr_vertex*2], self.tree(curr_vertex*2+1))
+            self.update(curr_vertex*2, segment_left, segment_middle, left, min(right, segment_middle), num_to_add)
+            self.update(curr_vertex*2+1, segment_middle + 1, segment_right, max(left, segment_middle+1), right, num_to_add)
+            self.tree[curr_vertex] = max(self.tree[curr_vertex*2], self.tree[curr_vertex*2+1])
 
     def query(self, curr_vertex, segment_left, segment_right, left, right):
-        if segment_left == segment_right:
+        if left > right:
             return float('-inf')
         
         if left == segment_left and right == segment_right:
             return self.tree[curr_vertex]
-        
+  
         self.push(curr_vertex)
         segment_middle = (segment_left + segment_right) // 2
         return max(self.query(curr_vertex*2, segment_left, segment_middle, left, min(right, segment_middle)),
                    self.query(curr_vertex*2+1, segment_middle+1, segment_right, max(left, segment_middle+1), right))
+    
+ 
 
+for test_num in range(1,4):
+    print(f"TESTING VARIANT {test_num}:")
+    test_fp = f"io/sample.in.{test_num}"
+    test_expected_fp = f"io/sample.out.{test_num}"
+
+    with open(test_fp, 'r') as input, open(test_expected_fp, 'r') as expected_output:
+        V = int(input.readline().strip())
+        N, Q = map(int, input.readline().strip().split())
+
+        input_list = list(map(int, input.readline().strip().split()))
+
+        match V:
+            case 1:
+                seg_tree = segment_tree_addition_and_get(input_list)
+            case 2: 
+                seg_tree = segment_tree_assignment_and_get(input_list)
+            case 3:
+                seg_tree = segment_tree_addition_and_max(input_list)
+
+        for q in range(Q):
+            line = input.readline().strip().split()
+            match(line[0]):
+                case "GET":
+                    res = seg_tree.get(1, 0, N-1, int(line[1]))
+                    expected = expected_output.readline().strip()
+                    print(f"Got value {res} (Expected: {expected}))")
+                    assert(res == int(expected))
+                case "UPDATE":
+                    print(f"Updating range {line[1]}-{line[2]} with value {line[2]}")
+                    seg_tree.update(1, 0, N-1, int(line[1]), int(line[2]), int(line[3]))
+                case "MAX":
+                    res = seg_tree.query(1, 0, N-1, int(line[1]), int(line[2]))
+                    expected = expected_output.readline().strip()
+                    print(f"Got value {res} (Expected: {expected}))")
+                    assert(res == int(expected))
