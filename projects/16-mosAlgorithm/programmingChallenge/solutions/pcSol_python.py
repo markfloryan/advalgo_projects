5613a0290eb1df57971f7be9f0ed0ba76982622b
Sebastian Condyles <zds4gv@virginia.edu>
2025-04-25 16:51:19 -0400

    fixed efficiency issues

--- projects/16-mosAlgorithm/programmingChallenge/solutions/pcSol_python.py
+++ projects/16-mosAlgorithm/programmingChallenge/solutions/pcSol_python.py
@@ -1,11 +1,29 @@
 import functools
 from math import floor, sqrt
+import bisect
+
+
 
 
 def sort_with_block_size(r1, r2, block_size):
+    if r1 is None or r2 is None:
+        return 0
     r1lb = r1[0] // block_size
     r2lb = r2[0] // block_size
     return r1lb - r2lb if r1lb != r2lb else r1[1] - r2[1]
+
+def translate_timestamp_queries_to_messages(queries, messages):
+    timestamps = [message[0] for message in messages]
+    indexed_queries = []
+    for l_ts, r_ts in queries:
+        l_idx = bisect.bisect_left(timestamps, l_ts)
+        r_idx = bisect.bisect_right(timestamps, r_ts) - 1
+        # subtract bisect right by 1 so we have the correct inclusive right index instead of the one after
+        if l_idx <= r_idx:
+            indexed_queries.append((l_idx, r_idx))
+        else:
+            indexed_queries.append(None)
+    return indexed_queries
 
 class Mo:
     def __init__(self, block_size, data):
@@ -13,8 +31,8 @@ class Mo:
         self.data = data # data is an object that has init(), add(idx), remove(idx), and answer() functions
 
     def query(self, queries):
-        queriesWithIdx = [(queries[i][0], queries[i][1], i) for i in range(len(queries))]
-        sortedQueries = sorted(queriesWithIdx, key = functools.cmp_to_key(lambda r1, r2 : sort_with_block_size(r1, r2, self.block_size)))
+        queries_with_idx = [(query[0], query[1], index) if query is not None else (-1,-1, index) for index, query in enumerate(queries)]
+        sortedQueries = sorted(queries_with_idx, key = functools.cmp_to_key(lambda r1, r2 : sort_with_block_size(r1, r2, self.block_size)))
 
         results = list(range(len(queries)))
 
@@ -22,7 +40,10 @@ class Mo:
 
         l = 0
         r = -1
-        for q in sortedQueries:
+        for index, q in enumerate(sortedQueries):
+            if q[0] == -1 and q[1] == -1:
+                results[q[2]] = 0
+                continue
             while q[0] < l:
                 l -= 1
                 self.data.add(l)
@@ -44,26 +65,25 @@ class Mo:
 class UniqueUsersData:
     def __init__(self, messages):
         self.messages = messages
+        self.messages.sort(key=lambda x: x[0])
 
     def init(self):
         self.uniqueUsers = {}
 
-    def add(self, idx):
-        if idx in messages:
-            name = messages[idx]
-            if name not in self.uniqueUsers:
-                self.uniqueUsers[name] = 1
-            else:
-                self.uniqueUsers[name] += 1
 
+    def add(self, idx):
+        name = self.messages[idx][1]
+        if name not in self.uniqueUsers:
+            self.uniqueUsers[name] = 1
+        else:
+            self.uniqueUsers[name] += 1
 
 
     def remove(self, idx):
-        if idx in messages:
-            name = messages[idx]
-            self.uniqueUsers[name] -= 1
-            if self.uniqueUsers[name] == 0:
-                del self.uniqueUsers[name]
+        name = self.messages[idx][1]
+        self.uniqueUsers[name] -= 1
+        if self.uniqueUsers[name] == 0:
+            del self.uniqueUsers[name]
 
 
     def answer(self):
@@ -74,12 +94,12 @@ first_line = first_line.split()
 num_messages = int(first_line[0])
 num_queries = int(first_line[1])
 
-messages = {}
+messages = []
 
 for i in range(num_messages):
     new_message = input()
     new_message = new_message.split()
-    messages[int(new_message[0])] = new_message[1]
+    messages.append((int(new_message[0]),new_message[1]))
 
 queries = []
 
@@ -88,10 +108,13 @@ for i in range(num_queries):
     new_query = new_query.split()
     queries.append((int(new_query[0]), int(new_query[1])))
 
+indexed_queries = translate_timestamp_queries_to_messages(queries, messages)
+
+
 data = UniqueUsersData(messages)
 block_size = floor(sqrt(num_messages))
 mo = Mo(block_size, data)
-results = mo.query(queries)
+results = mo.query(indexed_queries)
 
 for i in range(len(results)):
     print(queries[i], ":", results[i])
