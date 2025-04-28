583033f11079fe14639c66c73d1007b5f59f91ef
ColeFrankenhoff <johndoe@example.com>
2025-04-28 01:23:53 -0400

    match java file and class name

--- projects/16-mosAlgorithm/programmingChallenge/solutions/pcSol_java.java
+++ projects/16-mosAlgorithm/programmingChallenge/solutions/pcSol_java.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class PcSolJava {
     // simple pair to hold timestamp + username
     static class Message {
         int timestamp;
